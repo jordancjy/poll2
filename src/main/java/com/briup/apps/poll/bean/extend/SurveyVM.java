e32a4c292ff9e5fe86da932a15695498b0154554
DELL <DELL@DESKTOP-PKON2S2>
2018-06-29 19:07:12 +0800

    添加survey 的get和set方法

--- src/main/java/com/briup/apps/poll/bean/extend/SurveyVM.java
+++ src/main/java/com/briup/apps/poll/bean/extend/SurveyVM.java
@@ -10,6 +10,54 @@ public class SurveyVM {
 	private String surveydate;
 	//关系
 	private Course course;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getSurveydate() {
+		return surveydate;
+	}
+	public void setSurveydate(String surveydate) {
+		this.surveydate = surveydate;
+	}
+	public Course getCourse() {
+		return course;
+	}
+	public void setCourse(Course course) {
+		this.course = course;
+	}
+	public ClazzVM getClazzVM() {
+		return clazzVM;
+	}
+	public void setClazzVM(ClazzVM clazzVM) {
+		this.clazzVM = clazzVM;
+	}
+	public User getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(User teacher) {
+		this.teacher = teacher;
+	}
+	public QuestionnaireVM getQnVM() {
+		return qnVM;
+	}
+	public void setQnVM(QuestionnaireVM qnVM) {
+		this.qnVM = qnVM;
+	}
 	private ClazzVM clazzVM;
 	private User teacher;
 	private QuestionnaireVM qnVM;
